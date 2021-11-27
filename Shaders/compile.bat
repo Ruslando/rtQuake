0d93d5c16ca4f78b164006c3d49620b498257e85
Ruslan Novikov <ruslan.novikov@hotmail.de>
2021-11-27 13:15:28 +0100

    adjusted compile.bat to add spirv conversion for raygen shaders

--- Shaders/compile.bat
+++ Shaders/compile.bat
@@ -12,7 +12,7 @@ call "%VS110COMNTOOLS%\VsDevCmd.bat"
 )
 
 if not exist bintoc.exe ( 
-cl.exe /nologo bintoc.c 
+	cl.exe /nologo bintoc.c 
 )
 
 for %%f in (*.vert) do (
@@ -25,12 +25,19 @@ for %%f in (*.frag) do (
 	bintoc.exe Compiled/%%~nf.fspv %%~nf_frag_spv > Compiled/%%~nf_frag.c
 )
 
+for %%f in (*.rgen, *.rchit, *.rmiss) do (
+	%VULKAN_SDK%\bin\glslangValidator.exe --target-env vulkan1.2 %%f -o Compiled/%%~nf.rspv
+	bintoc.exe Compiled/%%~nf.rspv %%~nf_ray_spv > Compiled/%%~nf_ray.c
+)
+
 for %%f in (*.comp) do (
 	set "file=%%f"
 	If not "!file!"=="!file:sops=!" (
-		%VULKAN_SDK%\bin\glslangValidator.exe --target-env vulkan1.1 -V %%f -o Compiled/%%~nf.cspv
+		%VULKAN_SDK%\bin\glslangValidator.exe --target-env vulkan1.2 -V %%f -o Compiled/%%~nf.cspv
 	) else (
 		%VULKAN_SDK%\bin\glslangValidator.exe -V %%f -o Compiled/%%~nf.cspv
 	)
 	bintoc.exe Compiled/%%~nf.cspv %%~nf_comp_spv > Compiled/%%~nf_comp.c
 )
+
+cmd /k
